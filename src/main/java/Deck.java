41db9ec26401babde1872e9c8da332af1bff3f0e
Kira Loo <youhavebeenkiraed@gmail.com>
2018-02-16 14:00:21 -0800

    Card properties.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,12 @@
 public class Deck {
+    private String[] suits = {"♣ Clubs", "♦ Diamonds", "❤ Hearts", "♠ Spades"};
+    private String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    public int sv = 2;
+
+    Deck() {
+
+    }
+
+    
+
 }
